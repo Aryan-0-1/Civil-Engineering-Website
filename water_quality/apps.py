fac2e0177cdaf0c8c9e2b4169c4a14468c7dd354
Aryan Choudhary <aryanchoudhary84010@gmail.com>
2026-01-02 20:43:08 +0530

    Delete water_quality directory

--- water_quality/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WaterQualityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'water_quality'
--- water_quality/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.water_quality, name='water_quality'),
-    path('water_predict',views.water_predict,name='water_predict'),
-]
