7557480439ce1d5bace54723973e67b071e14220
Aryan Choudhary <aryanchoudhary84010@gmail.com>
2026-01-02 21:21:47 +0530

    Remove water_quality URL path from urls.py
    Remove the water_quality URL path from routing.

--- Civil/urls.py
+++ Civil/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('opencf/',include('ocf.urls')),
     path('pipe/',include('pipe.urls')),
     path('rect/',include('rect.urls')),
-    path('water_quality/',include('water_quality.urls')),
 
     # path('design/', include('design.urls'))
 ]
